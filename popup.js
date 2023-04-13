e0cd04d31575e107ac868d396bb703e9364d084d
rahul0x00 <rahul0x00@gmail.com>
2023-04-13 11:31:17 +0530

    fixed errors

--- popup.js
+++ popup.js
@@ -1,28 +1,36 @@
-// Get the note from local storage and display it in the textarea
-function getNoteFromStorage() {
-  chrome.storage.sync.get(['note'], function(result) {
-    var note = result.note;
-    if (note != undefined) {
-      document.getElementById('note').value = note;
+const noteInput = document.getElementById("note");
+const saveButton = document.getElementById("save");
+const savedNote = document.getElementById("saved-note");
+
+// Get the saved note from storage when the popup is opened
+getFromStorage(function(note) {
+  savedNote.innerText = note;
+});
+
+// Save the note to storage when the save button is clicked
+saveButton.addEventListener("click", function() {
+  const note = noteInput.value;
+  saveToStorage(note, function() {
+    savedNote.innerText = note;
+  });
+});
+
+function saveToStorage(note, callback) {
+  chrome.storage.sync.set({note: note}, function() {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+    } else {
+      console.log("Note saved");
+    }
+    if (callback) {
+      callback();
     }
   });
 }
 
-// Save the note to local storage when the save button is clicked
-document.getElementById('save').addEventListener('click', function() {
-  var note = document.getElementById('note').value;
-  chrome.storage.sync.set({'note': note}, function() {
-    console.log('Note saved:', note);
+function getFromStorage(callback) {
+  chrome.storage.sync.get("note", function(result) {
+    const note = result.note || "";
+    callback(note);
   });
-});
-
-// Update the note in the textarea whenever it is changed
-document.getElementById('note').addEventListener('input', function() {
-  var note = document.getElementById('note').value;
-  chrome.storage.sync.set({'note': note}, function() {
-    console.log('Note updated:', note);
-  });
-});
-
-// Call getNoteFromStorage to load the note on page load
-getNoteFromStorage();
+}
